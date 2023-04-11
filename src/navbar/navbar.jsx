e5abd20dc7713fa5d6e021eb630be5097088b3ac
IRoan <contact@iroan.net>
2023-04-11 18:53:43 +0200

    Initial commit

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,13 +1,13 @@
 import styles from "./navbar.module.scss";
 import logo_webp from "./logo.webp"
-import {BsGithub} from 'react-icons/Bs';
+import {TfiGithub} from 'react-icons/Tfi';
 
 function Navbar() {
     return (
         <nav>
             <div className={styles.logo}><img alt={"RVWLogo"} src={logo_webp}/></div>
             <ul>
-                <li><a href="#"><BsGithub/></a></li>
+                <li><a href="https://github.com/IIRoan/ApexMapRotationWeb"><TfiGithub/></a></li>
             </ul>
         </nav>
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     build: {
         rollupOptions: {
             external: [
-
             ]
         }
     }
