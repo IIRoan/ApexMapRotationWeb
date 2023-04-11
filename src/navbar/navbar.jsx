197acbad0b64c1683fe8bb176176cf2a0cbe42b1
IRoan <contact@iroan.net>
2023-04-11 18:54:16 +0200

    Initial commit

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,13 +1,12 @@
 import styles from "./navbar.module.scss";
 import logo_webp from "./logo.webp"
-import {TfiGithub} from 'react-icons/Tfi';
 
 function Navbar() {
     return (
         <nav>
             <div className={styles.logo}><img alt={"RVWLogo"} src={logo_webp}/></div>
             <ul>
-                <li><a href="https://github.com/IIRoan/ApexMapRotationWeb"><TfiGithub/></a></li>
+                <li><a href="https://github.com/IIRoan/ApexMapRotationWeb">hi</a></li>
             </ul>
         </nav>
 
